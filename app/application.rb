5a3bd7d7c0032d02c126ef32ac7508a938de9b3c
Alexander Rovang <wolf_vii@yahoo.com>
2021-01-20 22:54:29 -0500

    commit tree c6ffa739b89c8bb1c8bdee0f0d8d3ca896bef0eb

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,9 @@
+require "pry"
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
@@ -10,6 +13,27 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+    elsif req.path.match(/cart/)
+      if @@cart != []
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      else
+        resp.write "Your cart is empty"
+      end
+
+    elsif req.path.match(/add/) 
+      search_term = req.params
+      item = search_term["item"]
+      if @@items.include?(item)
+        resp.write "added #{item}"
+        @@cart << item
+      else
+        resp.write "We don't have that item"
+      end
+      #This should check to see if that item is in @@items 
+      #and then add it to the cart if it is. 
+      #Otherwise give an error
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
